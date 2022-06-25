2068757166b864d7dca61762acdec60faefa06db
Viviane Puga <vnpuga@gmail.com>
2022-06-25 16:13:06 -0300

    feat: exercicios de fixacao

---
+++ curso-javascript/modulos/main.mjs
@@ -0,0 +1,7 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Viviane', 41));
+console.log(mostraCidade('Viviane', 'Valentim Gentil'));
+console.log(mostraHobby('Viviane', 'assistir séries'));
+
+//feito em aula com Stephany Nusch
